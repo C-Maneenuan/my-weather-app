4c8232b06b9c03b2ce044dadaa6fd0c4fcb23d5a
C-Maneenuan <focus37976@gmail.com>
2025-08-17 19:45:17 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,47 @@
-const apiKey = '6826282b88554732451441bbd3dffd28';
+const apiKey = '3b771ff9ce752ffaa10ee98cc4342d5e';
 
 const searchForm = document.querySelector('#search-form');
 const cityInput = document.querySelector('#city-input');
 const weatherInfoContainer = document.querySelector('#weather-info-container');
+searchForm.addEventListener('submit', (event) => {
+    event.preventDefault();
+
+    const cityName = cityInput.value.trim();
+
+    if (cityName) {
+        getWeather(cityName);
+    } else {
+        alert('Insert City Name');
+    }
+});
+
+async function getWeather(city) {
+    weatherInfoContainer.innerHTML = `<p>รอกำเน้อ...</p>`;
+    const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric&lang=th`;
+
+    try {
+        const response = await fetch(apiUrl);
+        if (!response.ok) {
+            throw new Error('สุมาเตอะฮาเซาะบ่ปะ');
+        }
+        const data = await response.json();
+        displayWeather(data);
+    } catch (error) {
+        weatherInfoContainer.innerHTML = `<p class="error">${error.message}</p>`;
+    }
+}
+
+function displayWeather(data) {
+    const { name, main, weather } = data;
+    const { temp, humidity } = main;
+    const { description, icon } = weather[0];
+
+    const weatherHtml = `
+        <h2>${name}</h2>
+        <img src="https://openweathermap.org/img/wn/${icon}@2x.png" alt="${description}">
+        <p style="font-size: 2rem; font-weight: bold;">${temp.toFixed(1)}°C</p>
+        <p>${description}</p>
+        <p>ความจื้น: ${humidity}%</p>
+    `;
+    weatherInfoContainer.innerHTML = weatherHtml;
+}
