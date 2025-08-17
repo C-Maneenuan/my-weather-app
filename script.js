088e5393f5aa5d8ca7e196e19d071fcc48dfb388
C-Maneenuan <focus37976@gmail.com>
2025-08-17 19:36:20 +0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,5 @@
+const apiKey = '6826282b88554732451441bbd3dffd28';
+
+const searchForm = document.querySelector('#search-form');
+const cityInput = document.querySelector('#city-input');
+const weatherInfoContainer = document.querySelector('#weather-info-container');
